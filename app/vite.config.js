bac8ebf35e8dbe0ee7dfffd08a9f671a4cd8db1a
AlexFernandes04 <everything.alex04@gmail.com>
2026-06-06 10:11:29 -0400

    more custom domain slop

--- app/vite.config.js
+++ app/vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/urinal-machine/',
+  base: '/',
   plugins: [react(), tailwindcss()],
   server: {
     port: 3000, // Set your desired port here
