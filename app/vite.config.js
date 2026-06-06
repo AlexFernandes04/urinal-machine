6015c401279bc4b75d861bc8a3c852efc4676aea
AlexFernandes04 <everything.alex04@gmail.com>
2026-06-05 22:13:59 -0400

    updated build

--- app/vite.config.js
+++ app/vite.config.js
@@ -4,11 +4,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/urinal-machine/',
   plugins: [react(), tailwindcss()],
   server: {
     port: 3000, // Set your desired port here
     allowedHosts: ['urinal-machine-production.up.railway.app', 'urinal-machine.onrender.com', 'https://alexfernandes04.github.io/urinal-machine/'], // Allow ngrok subdomains
   },
-    base: '/urinal-machine/'
 
 })
